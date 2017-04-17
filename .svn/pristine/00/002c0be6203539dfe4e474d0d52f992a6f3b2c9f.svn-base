52cf347cce54205e8e5d3caea9bcc69d090d4089
yuanlin12 <416306434@qq.com>
2017-04-17 10:24:19 +0800

    项目提交

---
+++ .svn/pristine/00/002c0be6203539dfe4e474d0d52f992a6f3b2c9f.svn-base
@@ -0,0 +1,5 @@
+package com.zw.as;
+
+public class Test2 {
+
+}
---
+++ customer/src/com/zw/as/Test.java
@@ -0,0 +1,10 @@
+package com.zw.as;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("123");
+	}
+
+}
